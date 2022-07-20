d81feae0da64fa1122b94ec852e7928aca5b955e
Saleh <62785016+SALEH-v@users.noreply.github.com>
2022-07-20 10:07:33 +0300

    c

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
                 </div>
 
                 <div className="header-username">
-                    <p>SS</p>
+                    <p>Saleh</p>
                     <div className="logout-container">
                         <button id="logout-button" onClick={logout}>Logout</button>
                     </div>
